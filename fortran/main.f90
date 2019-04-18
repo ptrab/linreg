dd87e67cfcdecc4706ea8086f37729f3b945f562
Philipp Traber <philipp.traber@uni-jena.de>
2019-04-18 12:24:43 +0200

    improved documentation

--- fortran/main.f90
+++ fortran/main.f90
@@ -1,36 +1,54 @@
 module allRoutines
+    !! Contains all the routines that are needed for the calculation.
+
     implicit none
 
-    !- proper specification of precision as of:
-    !-   http://fortranwiki.org/fortran/show/Real+precision
+    ! Definition of double precision
     integer, parameter :: dp = kind(0.d0)
+        !! Proper specification of precision as of:
+        !! http://fortranwiki.org/fortran/show/Real+precision
 
-    !- variables of type: INTEGER
-    !- i, j and k are simple counters
-    integer :: i, j, k, sOne
-    !- the number of input blocks ... most probably 1
-    !- if no averaging over multiple groups is wanted
-    !- or if for one concentration an outlier has been removed
+    ! Variables of type: INTEGER
+    integer :: i, j, k
+        !! i, j and k are simple counters
+
+    integer :: sOne
+        !! sOne is the number of all value triples; nConc'.nTimes
+
     integer :: nBlocks
-    !- the number of concentrations and measure point times per block
-    integer, dimension(:), allocatable  :: nConc, nTimes
+        !! The number of input blocks
+        !! Most probably 1 if no averaging over multiple groups is wanted
+        !! or if for one concentration an outlier has been removed
 
-    !- variables of type: REAL (single precision)
-    !- different sums over c and x, c and x and y, etc.
-    !- the regression parameters And B with their variances sA and sB
-    !- some further variance numbers and related parameter sSqRes and s
-    !- some numbers related to the coefficient of determination r; c0mean and ssTot
-    real(dp) :: Scx, Scxy, Sc2x2, Sy, A, B, sSqRes, s, sA, sB, c0mean, ssTot, rSq
-    !- different sums for the calculation of the slopes in calcMeanSlopes
-    real(dp) :: Sx, Sx2, Sxy, meanSlope
-    !- the arrays (matrices) holding the concentrations, kappa_0, kappa_inf
-    !- as well as the measure point times ber block
+    integer, dimension(:), allocatable  :: nConc, nTimes
+        !! The number of concentrations and measure point times per block
+
+    ! Variables of type: REAL
+    real(dp) :: sum_ct, sum_ctc0pc, sum_c2t2, sum_c0pc
+        !! Different sums over c and x, c and x and y, etc.
+    
+    real(dp) :: A, B, varA, varB
+        !! The regression parameters A and B with their variances varA and varB
+    
+    real(dp) :: sum_res2, s
+        !! Some further variance numbers and related parameter sum_res2 and s
+    
+    real(dp) :: c0mean, ssTot, rSq
+        !! Some numbers related to the coefficient of determination r; c0mean and ssTot
+
+    real(dp) :: sum_t, sum_t2, sum_tc0pc, meanSlope
+        !! Different sums for the calculation of the slopes in calcMeanSlopes
+
     real(dp), dimension(:, :), allocatable   :: conc, kappa0, kappaInf, times
-    !- the arrays (tensors) holding the measured kappa values and the
-    !- converted c_0/c values based on them ... per block
+        !! The arrays (matrices) holding the concentrations, kappa_0, kappa_inf
+        !! as well as the measure point times ber block
+
     real(dp), dimension(:, :, :), allocatable :: kappa, c0pc
+        !! The arrays (tensors) holding the measured kappa values and
+        !! the converted c_0/c values based on them ... per block
 
     logical, parameter :: DEBUG = .FALSE.
+        !! A debug parameter that add additional output
 
     private
     public parseInput, convertKappa, calcSums, calcAB, calcVariance, calcMeanSlope, &
@@ -60,32 +78,32 @@ contains
         ! 4.6 4.45 4.31 4.15 4.07 3.97 3.86 3.78 3.7 3.62 3.5 3.39 3.29 3.2
         ! 6.84 6.47 6.2 5.97 5.76 5.56 5.41 5.27 5.13 5 4.81 4.65 4.5 4.37
 
-        !- open input file
         open (1, file="data")
 
-        !!! "STATIC" INPUT
-        !- "static" in a sense of that these need to be present
-        !- to define which part of the further input is being
-        !- taken into account and what not
+        ! "STATIC" INPUT
+        ! --------------
+        ! "static" in a sense of that these need to be present
+        ! to define which part of the further input is being
+        ! taken into account and what not
 
-        !- get number of input blocks
         read (1, *) nBlocks
+            !! Get the number of input blocks
 
-        !- allocate arrays for numbers of concentrations
-        !- and numbers of times per block
+        ! allocate arrays for numbers of concentrations
+        ! and numbers of times per block
         allocate (nConc(nBlocks))
         allocate (nTimes(nBlocks))
 
-        !- get number of concentrations (aka lines) per block
+        ! get number of concentrations (aka lines) per block
         read (1, *) nConc
-        !-  get number of measure points per block
+        !  get number of measure points per block
         read (1, *) nTimes
 
-        !- allocate matrices and tensors for:
-        !- concentrations per block,
-        !- measured kappa, kappa_0 and kappa_inf per block,
-        !- the block's time points for measures,
-        !- converted kapp> -> c_0/c values for each block
+        ! allocate matrices and tensors for:
+        ! concentrations per block,
+        ! measured kappa, kappa_0 and kappa_inf per block,
+        ! the block's time points for measures,
+        ! converted kapp> -> c_0/c values for each block
         allocate (conc(nBlocks, maxval(nConc)))
         allocate (kappa0(nBlocks, maxval(nConc)))
         allocate (kappaInf(nBlocks, maxval(nConc)))
@@ -93,24 +111,25 @@ contains
         allocate (kappa(nBlocks, maxval(nConc), maxval(nTimes)))
         allocate (c0pc(nBlocks, maxval(nConc), maxval(nTimes)))
 
-        !!! "DYNAMIC" INPUT
-        !- "dynamic" in a sense of that these are the variable
-        !- measure points and measure values that vary from time to time
+        ! "DYNAMIC" INPUT
+        ! ---------------
+        ! "dynamic" in a sense of that these are the variable
+        ! measure points and measure values that vary from time to time
         do i = 1, nBlocks
             read (1, *) conc(i, 1:nConc(i))
             read (1, *) kappa0(i, 1:nConc(i))
             read (1, *) kappaInf(i, 1:nConc(i))
             read (1, *) times(i, 1:nTimes(i))
-            !- read measure points
+            ! read measure points
             do j = 1, nConc(i)
                 read (1, *) kappa(i, j, 1:nTimes(i))
             end do
         end do
 
-        !- close input file
+        ! close input file
         close (1)
 
-        !- show input
+        ! show input
         if (DEBUG .eqv. .TRUE.) then
             do i = 1, nBlocks
                 print *, '> Block', i
@@ -125,12 +144,12 @@ contains
     end subroutine
 
     subroutine convertKappa
-        !- this subroutine converts the measured kappas to a relative concentration
-        !-
-        !-    c_0     kappa_0 - kappa_inf
-        !-   ----- = ---------------------
-        !-     c       kappa  - kappa_inf
-        !-
+        ! this subroutine converts the measured kappas to a relative concentration
+        !
+        !    c_0     kappa_0 - kappa_inf
+        !   ----- = ---------------------
+        !     c       kappa  - kappa_inf
+        !
         real(dp) :: tmp1, tmp2
 
         do i = 1, nBlocks
@@ -155,61 +174,91 @@ contains
     end subroutine
 
     subroutine calcSums
-        !- the variables are explained at the beginning of the module
-        sOne = dot_product(nConc, nTimes)
-        !- how to calculate the sums is explained in the following loop
-        Sy = 0.0_dp
-        Scx = 0.0_dp
-        Scxy = 0.0_dp
-        Sc2x2 = 0.0_dp
+        ! the variables are explained at the beginning of the module
 
-        !- iterate over all input blocks
+        sOne = dot_product(nConc, nTimes)
+            !! the number of all value triples, nConc'.nTimes
+
+        sum_c0pc = 0.0_dp
+            !! the sum of all (c_0/c) values
+            !! $$\sum_{ijk} \left(\frac{c_0}{c}\right)_{ijk}$$
+
+        sum_ct = 0.0_dp
+            !! sum all concentrations * times
+            !! $$\sum_{ijk} c_{ij} * t_{ik}$$
+
+        sum_ctc0pc = 0.0_dp
+            !! sum all concentrations * times * rel concentrations
+            !! $$\sum_{ijk} c_{ij} * t_{ik} * \left(\frac{c_0}{c}\right)_{ijk}$$
+
+        sum_c2t2 = 0.0_dp
+            !! sum all squared concentrations and squared times
+            !! $$\sum_{ijk} c_{ij}^2 * t_{jk}^2$$
+
+        ! iterate over all input blocks
         do i = 1, nBlocks
 
-            !- iterate over all measure series per block
+            ! iterate over all measure series per block
             do j = 1, nConc(i)
 
-                !- iterate over all measure points per block and concentration
+                ! iterate over all measure points per block and concentration
                 do k = 1, nTimes(i)
 
-                    !- sum all relative concentrations c_0/c:
-                    !-   sum_i=1^M sum_j=1^(N_i) (c_0/c)_ij
-                    Sy = Sy + c0pc(i, j, k)
+                    sum_c0pc = sum_c0pc + c0pc(i, j, k)
 
-                    !- sum all concentrations * times
-                    !-   sum_i=1^M sum_j=1^(N_i) c_i * t_j
-                    Scx = Scx + conc(i, j) * times(i, k)
+                    sum_ct = sum_ct + conc(i, j) * times(i, k)
 
-                    !- sum all concentrations * times * rel concentrations
-                    !-   sum_i=1^M sum_j=1^(N_i) c_i * t_j * (c_0/c)_ij
-                    Scxy = Scxy + conc(i, j) * times(i, k) * c0pc(i, j, k)
+                    sum_ctc0pc = sum_ctc0pc + conc(i, j) * times(i, k) * c0pc(i, j, k)
 
-                    !- sum all squared concentrations and squared times
-                    !-   sum_i=1^M sum_j=1^(N_i) c_i^2 * t_j^2
-                    Sc2x2 = Sc2x2 + conc(i, j)**2 * times(i, k)**2
+                    sum_c2t2 = sum_c2t2 + conc(i, j)**2 * times(i, k)**2
                 end do
             end do
         end do
 
-        !- print intermediate results
+        ! print intermediate results
         if (DEBUG .eqv. .TRUE.) then
             write (*, '(A, I15.0)') 's1:                 ', sOne
-            write (*, '(A, F15.7)') 'sum(c_0/c):         ', Sy
-            write (*, '(A, F15.7)') 'sum(c * t):         ', Scx
-            write (*, '(A, F15.7)') 'sum(c * t * c_0/c): ', Scxy
-            write (*, '(A, F15.7)') 'sum(c^2 * t^2):     ', Sc2x2
+            write (*, '(A, F15.7)') 'sum(c_0/c):         ', sum_c0pc
+            write (*, '(A, F15.7)') 'sum(c * t):         ', sum_ct
+            write (*, '(A, F15.7)') 'sum(c * t * c_0/c): ', sum_ctc0pc
+            write (*, '(A, F15.7)') 'sum(c^2 * t^2):     ', sum_c2t2
             write (*, *) ''
         end if
     end subroutine
 
     subroutine calcAB
-        !- intercept A
-        A = (Scx * Scxy - Sc2x2 * Sy) / (Scx**2 - Sc2x2 * sOne)
+        A = (sum_ct * sum_ctc0pc - sum_c2t2 * sum_c0pc) / (sum_ct**2 - sum_c2t2 * sOne)
+            !! Intercept A
+            !! \[
+            !!     A = \frac{
+            !!               \sum_{ijk} c_{ij} t_{ik}
+            !!             * \sum_{ijk} c_{ij} t_{ik} \left(\frac{c_0}{c}\right)_{ijk}
+            !!             - \sum_{ijk} c_{ij}^2 t_{ik}^2
+            !!             * \sum_{ijk} \left(\frac{c_0}{c}\right)_{ijk}
+            !!              }
+            !!              {
+            !!               \left( \sum_{ijk} c_{ij} t_{ik} \right)^2
+            !!             - \sum_{ijk} c_{ij}^2 t_{ik}^2
+            !!             * \sum_{ijk} 1
+            !!              }
 
-        !- slope B
-        B = (Sy * Scx -  sOne * Scxy) / (Scx**2 - Sc2x2 * sOne)
+        B = (sum_c0pc * sum_ct -  sOne * sum_ctc0pc) / (sum_ct**2 - sum_c2t2 * sOne)
+            !! Slope B
+            !! \[
+            !!     B = \frac{
+            !!               \sum_{ijk} \left(\frac{c_0}{c}\right)_{ijk}
+            !!             * \sum_{ijk} c_{ij} t_{ik}
+            !!             - \sum_{ijk} 1
+            !!             * \sum_{ijk} c_{ij} t_{ik} \left(\frac{c_0}{c}\right)_{ijk}
+            !!              }
+            !!              {
+            !!               \left( \sum_{ijk} c_{ij} t_{ik} \right)^2
+            !!             - \sum_{ijk} c_{ij}^2 t_{ik}^2
+            !!             * \sum_{ijk} 1
+            !!              }
+            !! \]
 
-        !- print intermediate results
+        ! print intermediate results
         if (DEBUG .eqv. .TRUE.) then
             write (*, '(A, 2F15.7)') 'A and B: ', A, B
             write (*, *) ''
@@ -218,70 +267,86 @@ contains
 
     subroutine calcVariance
         real(dp) :: tmp
+            !! A temporary variable
 
-        !- sum of squared residues
-        sSqRes = 0.0_dp
+        sum_res2 = 0.0_dp
+            !! Sum of squared residuals
+            !! \[
+            !!     \sum_{ijk} \left(
+            !!                    \left(\frac{c_0}{c}\right)_{ijk}
+            !!                  - A
+            !!                  - c_{ij} * B * t_{ik}
+            !!                \right)^2
+            !! \]
+
         do i = 1, nBlocks
 
             do j = 1, nConc(i)
 
                 c0mean = sum(c0pc(i, j, :)) / nTimes(i)
+                    !! The mean of a measure series
+
                 do k = 1, nTimes(i)
                     tmp = c0pc(i, j, k) - A - conc(i, j) * B * times(i, k)
-                    sSqRes = sSqRes + tmp**2
+                    sum_res2 = sum_res2 + tmp**2
+
                     tmp = c0pc(i, j, k) - c0mean
                     ssTot = ssTot + tmp**2
                 end do
             end do
         end do
 
-        !- Reststreuung ... leftover(?) variance
-        s = sqrt(sSqRes / (sOne - 2.0_dp * sum(nConc)))
+        s = sqrt(sum_res2 / (sOne - 2.0_dp * sum(nConc)))
+            !! Unexplained variance (? ... Reststreuung)
 
-        !- variance of A
-        sA = s * sqrt(Sc2x2 / (sOne * Sc2x2 - Scx**2))
+        varA = s * sqrt(sum_c2t2 / (sOne * sum_c2t2 - sum_ct**2))
+            !! Variance of intercept A
 
-        !- variance of B
-        sB = s * sqrt(sOne / (sOne * Sc2x2 - Scx**2))
+        varB = s * sqrt(sOne / (sOne * sum_c2t2 - sum_ct**2))
+            !! Variance of slope B
 
-        !- coefficient of determination, R^2
-        rSq = 1.0_dp - sSqRes / ssTot
+        rSq = 1.0_dp - sum_res2 / ssTot
+            !! Coefficient of determination
 
-        !- print intermediate results
+        ! print intermediate results
         if (DEBUG .eqv. .TRUE.) then
-            write (*, '(A, F15.7)') 'sum(res^2): ', sSqRes
+            write (*, '(A, F15.7)') 'sum(res^2): ', sum_res2
             write (*, '(A, F15.7)') 's:          ', s
-            write (*, '(A, F15.7)') 's(A):       ', sA
-            write (*, '(A, F15.7)') 's(B):       ', sB
+            write (*, '(A, F15.7)') 's(A):       ', varA
+            write (*, '(A, F15.7)') 's(B):       ', varB
             write (*, '(A, F15.7)') 'R^2:        ', rSq
             write (*, *) ''
         end if
     end subroutine
 
     subroutine calcMeanSlope
-        !- this routine calculates not the slope for a combined fit
-        !- of all measure series but calculates the slope for each
-        !- of them an calculates the mean afterwards
+        !! Calculates the slope for each single measure series their mean slope
+
         integer :: cnt = 1
+            !! Simple counter
+
         real(dp) :: tmp
+            !! A temporary varuiable
+
         real(dp), dimension(sum(nConc)) :: allSlopes
+            !! The array that holds all slopes
 
         do i = 1, nBlocks
             do j = 1, nConc(i)
-                Sxy = 0.0_dp
-                Sx = 0.0_dp
-                Sx2 = 0.0_dp
-                Sy = 0.0_dp
+                sum_tc0pc = 0.0_dp
+                sum_t = 0.0_dp
+                sum_t2 = 0.0_dp
+                sum_c0pc = 0.0_dp
 
                 do k = 1, nTimes(i)
-                    Sx = Sx + times(i, k)
-                    Sx2 = Sx2 + times(i, k)**2
-                    Sxy = Sxy + times(i, k) * c0pc(i, j, k)
-                    Sy = Sy + c0pc(i, j, k)
+                    sum_t = sum_t + times(i, k)
+                    sum_t2 = sum_t2 + times(i, k)**2
+                    sum_tc0pc = sum_tc0pc + times(i, k) * c0pc(i, j, k)
+                    sum_c0pc = sum_c0pc + c0pc(i, j, k)
                 end do
 
-                tmp = nTimes(i) * Sxy - Sx * Sy
-                tmp = tmp / (nTimes(i) * Sx2 - (Sx)**2)
+                tmp = nTimes(i) * sum_tc0pc - sum_t * sum_c0pc
+                tmp = tmp / (nTimes(i) * sum_t2 - (sum_t)**2)
                 allSlopes(cnt) = tmp / conc(i, j)
                 cnt = cnt + 1
             end do
@@ -293,8 +358,8 @@ contains
     subroutine printResults
         write (*, '(A)') 'FITTING RESULTS'
         write (*, *) ''
-        write (*, '(A, 2F15.7)') '  Intersection A with s(A):   ', A, sA
-        write (*, '(A, 2F15.7)') '  Slope B with s(B):          ', B, sB
+        write (*, '(A, 2F15.7)') '  Intersection A with s(A):   ', A, varA
+        write (*, '(A, 2F15.7)') '  Slope B with s(B):          ', B, varB
         write (*, '(A,  F15.7)') '  Coef. of determination R^2: ', rSq
         write (*, *) ''
         write (*, '(A,  F15.7)') '  Mean slope of single fits:  ', meanSlope
@@ -317,27 +382,27 @@ program main
     use allRoutines
     implicit none
 
-    !- parse the input file
+    ! parse the input file
     call parseInput
 
-    !- convert measured kappas to concentration ratio c_0/c
+    ! convert measured kappas to concentration ratio c_0/c
     call convertKappa
 
-    !- calculate the sums
+    ! calculate the sums
     call calcSums
 
-    !- calculate fit parameters A and B
+    ! calculate fit parameters A and B
     call calcAB
 
-    !- calculate the mean of the slope for each measure series
+    ! calculate the mean of the slope for each measure series
     call calcMeanSlope
 
-    !- calculate variance parameters s, s(A), s(B) and r
+    ! calculate variance parameters s, s(A), s(B) and r
     call calcVariance
 
-    !- print the results
+    ! print the results
     call printResults
 
-    !- deallocate all arrays
+    ! deallocate all arrays
     call deallocAll
 end program
